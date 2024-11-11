173595019b84b38452edcb42d431d6baa4f38491
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-11-11 13:27:08 -0600

    Update page3.php

--- page3.php
+++ page3.php
@@ -1,1 +1,27 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Highcharts Area Chart</title>
+  <script src="https://code.highcharts.com/highcharts.js"></script>
+</head>
+<body>
+  <div id="highcharts-area" style="width: 600px; height: 400px;"></div>
+  <script>
+    Highcharts.chart('highcharts-area', {
+      chart: { type: 'area' },
+      title: { text: 'Website Traffic Over 6 Months' },
+      xAxis: {
+        categories: ['January', 'February', 'March', 'April', 'May', 'June'],
+        title: { text: 'Month' }
+      },
+      yAxis: {
+        title: { text: 'Visitors' }
+      },
+      series: [{
+        name: 'Visitors',
+        data: [800, 1200, 1000, 1800, 2000, 2300]
+      }]
+    });
+  </script>
+</body>
+</html>
