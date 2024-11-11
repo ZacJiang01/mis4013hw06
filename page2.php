cc3bf8efe8e3812e485bdf8e1314ad27210c9d7e
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-11-11 13:26:34 -0600

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,31 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>D3.js Example</title>
+  <title>D3.js Scatter Plot</title>
   <script src="https://d3js.org/d3.v7.min.js"></script>
 </head>
 <body>
-  <div id="d3-chart"></div>
+  <svg width="600" height="400"></svg>
   <script>
-    const data = [12, 19, 3, 5, 2, 3];
-    const width = 400;
-    const height = 200;
-    const barWidth = width / data.length;
+    const data = [
+      { x: 10, y: 20 }, { x: 20, y: 35 }, { x: 30, y: 50 },
+      { x: 40, y: 60 }, { x: 50, y: 80 }, { x: 60, y: 90 },
+    ];
 
-    const svg = d3.select("#d3-chart")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height);
+    const svg = d3.select("svg"),
+          width = +svg.attr("width"),
+          height = +svg.attr("height"),
+          margin = { top: 20, right: 20, bottom: 30, left: 40 };
 
-    svg.selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", (d, i) => i * barWidth)
-      .attr("y", d => height - d * 10)
-      .attr("width", barWidth - 2)
-      .attr("height", d => d * 10)
-      .attr("fill", "steelblue");
-  </script>
-</body>
-</html>
+    const x = d3.scaleLinear().domain([0, d3.max(data, d => d.x)]).range([margin.left, width - margin.right]);
+    const y = d3.scaleLinear().domain([0, d3.max(data, d => d.y)]).range([height - margin.bottom, margin.top]);
+
+    svg.append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x))
+      .append("text")
+      .attr("fill", "black")
+      .attr("x", width / 2)
+      .attr("y", 25)
+      .text("X Value");
+
+    svg.append("g")
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y))
+      .append("text")
+      .attr("fill", "black")
+      .attr("x", -20)
+      .attr("y", 15)
+      .text("Y Value");
+
+    svg.se
